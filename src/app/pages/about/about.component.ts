6f3c18e312e28a5889a5cbc6aeb99bc4574f1a18
Kazi Waziullah <waziullah@live.com>
2025-07-08 17:35:06 +0600

    update: Forms code uploaded

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,13 +1,43 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule,ReactiveFormsModule],
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent {
+  data: string[] = ['Apples', 'Oranges', 'Purple']
 
+  //For Forms Version #1
+  userObj = {
+    username: "wazi",
+    password: "12345678",
+    email: "w@gmail.com"
+  }
+
+  processData() {
+    console.log('Form submitted')
+    console.log("UserObj",this.userObj)
+  }
+
+   //For Forms Version #2
+   userSignupForm : FormGroup;
+
+   constructor(private formBuilder : FormBuilder){
+    this.userSignupForm = this.formBuilder.group({
+      username:['',Validators.required],
+      email:['',[Validators.required,Validators.email]],
+      password:['',[Validators.required,Validators.min(8)]]
+    });
+   }
+
+   onSubmit(){
+    console.log("Form submitted")
+    console.log("UserForm", this.userSignupForm)
+   }
 }
